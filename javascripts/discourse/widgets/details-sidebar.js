4969a4bcb0aca17aa46fb84867d12c3ef3321313
Jonay Rodríguez <jonay.rodriguez@marfeel.com>
2023-02-21 16:59:29 +0100

    fixes

--- javascripts/discourse/widgets/details-sidebar.js
+++ javascripts/discourse/widgets/details-sidebar.js
@@ -89,6 +89,10 @@ createWidget("details-sidebar", {
         const sidebar = document.querySelector(".sidebar-sections");
         const customSidebar = document.querySelector(".sidebar-sections .cooked");
 
+        if (customSidebar) {
+          customSidebar.remove();
+        }
+
         if (sidebar && !customSidebar && this.state?.posts && this.state.posts[0]?.attrs?.cooked) {
           const wrapper = document.createElement("div");
 
